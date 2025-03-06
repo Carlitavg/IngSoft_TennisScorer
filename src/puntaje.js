9e18e96cc63259c1b86303677a269517f06c4c95
Carla_Diana <carladianavgonzales@gmail.com>
2025-03-06 09:44:54 -0400

    Feat: funcionalidad 15 - 0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,4 +1,7 @@
-function puntuacion (a, b){
+function puntuacion (score1, score2){
+    if(score1 === 1 && score2 == 0){
+        return "15 - 0"
+    }
     return "Love - Love"
 }
 export default puntuacion;
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -4,6 +4,9 @@ describe("Marcador de puntaje de tenis", () => {
   it("en caso de que ambos jugadores tengan 0 el marcador es Love, Love", () => {
     expect(puntuacion(0, 0)).toEqual("Love - Love");
   });
+  it("en caso de que un jugador tenga 1 punto y el otro jugador tenga 0 puntos", () => {
+    expect(puntuacion(1, 0)).toEqual("15 - 0");
+  });
 });
 
 
